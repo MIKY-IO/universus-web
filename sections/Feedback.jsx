18a41e54b48a46ae1842065d48bca333b3885d25
Mikulas Richter <mikulasrichter@gmail.com>
2022-12-22 16:58:12 +0100

    Final

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -3,7 +3,8 @@
 import { motion } from "framer-motion";
 
 import styles from "../styles";
-import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
+import { fadeIn, staggerContainer } from "../utils/motion";
+// import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
 
 const Feedback = () => (
   <section className={`${styles.paddings}`}>
